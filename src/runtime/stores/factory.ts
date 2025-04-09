0455d22d593d277fd4f875e7b1ec60221fae5232
Billybobbonnet <boitalettre9@gmail.com>
2025-04-09 13:08:26 +0200

    fix: missing dependencies

--- src/runtime/stores/factory.ts
+++ src/runtime/stores/factory.ts
@@ -1,4 +1,5 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
+import { reactive, toRefs } from "vue";
 export const createDynamicStore = (storeName: string, moduleState: object) => {
   return defineStore(storeName, () => {
     const state = reactive({
@@ -10,10 +11,10 @@ export const createDynamicStore = (storeName: string, moduleState: object) => {
       search: "",
       error: null as string | null,
       ...moduleState,
-    })
+    });
 
     return {
       ...toRefs(state),
-    }
-  })
-}
+    };
+  });
+};
