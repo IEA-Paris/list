10e820675bd5c886f23bda62715f5a71955bbe90
Hamed Bouare <hamed.bouare@paris-iea.fr>
2025-06-06 09:30:22 +0200

    feat: added events

---
+++ playground/pages/events/[slug].vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <EventsView :item="tag" :loading="loading"></EventsView
+  ></v-container>
+</template>
+
+<script setup>
+const { $queries } = useNuxtApp();
+
+const { fetchItem } = useFetchItem();
+const loading = ref(true);
+// Fetch the item
+const { data: tag } = await useAsyncData("item", async () => {
+  return await fetchItem({
+    query: $queries.events.get,
+    key: "getEvent",
+  });
+});
+loading.value = false;
+</script>
