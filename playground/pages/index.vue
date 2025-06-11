31195da530cfa1b7cac52ae8cabac7e7beef3ec2
Hamed Bouare <hamed.bouare@paris-iea.fr>
2025-06-11 08:41:08 +0200

    feat: added users and apps

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ListOrganismsList type="events" />
-    <ListOrganismsList type="people" />
+    <!-- <ListOrganismsList type="people" /> -->
     <!--     <ListOrganismsList type="actions" /> -->
     <!-- <ListOrganismsList type="publications" />
     <ListOrganismsList type="projects" />
---
+++ src/runtime/components/users/View.vue
@@ -0,0 +1,22 @@
+<template>
+  {{ item }}
+</template>
+
+<script setup>
+import { useNuxtApp } from "#imports";
+
+const { $stores } = useNuxtApp();
+defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+$stores.users.loading = false;
+</script>
