61a5035f944ce8aebe814ca845c46e70553998c8
Hamed Bouare <hamed.bouare@paris-iea.fr>
2025-10-29 08:33:33 +0100

    fix: computeVisibility

--- src/runtime/components/list/molecules/Filters.vue
+++ src/runtime/components/list/molecules/Filters.vue
@@ -45,7 +45,7 @@ const props = defineProps(["type", "expanded"])
 
 const ComponentName = (name) => {
   return resolveComponent(
-    "ListInputs" + capitalize($stores[props.type].filters[name].type),
+    "ListInputs" + capitalize($stores[props.type].filters[name].type)
   )
 }
 const getItems = (name) => {
@@ -60,7 +60,7 @@ const getItems = (name) => {
         title: i18n.t(
           props.type === "people" && name === "vintage"
             ? item
-            : `list.filters.${props.type}.${name}.${item}`,
+            : `list.filters.${props.type}.${name}.${item}`
         ),
         value: item,
       }))
@@ -73,7 +73,7 @@ const getItems = (name) => {
   }
   // TODO replace with package based values
   return Object.keys(
-    messages.value[locale.value].list.filters[props.type][name],
+    messages.value[locale.value].list.filters[props.type][name]
   )
     .filter((key) => key !== "label")
     .map((item) => ({
@@ -82,24 +82,52 @@ const getItems = (name) => {
     }))
 }
 
-const computeVisibility = (filterItem) => {
-  return (
-    // if anything is set in the visibility key
-    !$stores[props.type].filters[filterItem].visibility ||
-    $stores[props.type].filters[filterItem].visibility?.default ||
-    $stores[props.type].filters[filterItem].visibility?.switchIf.find(
-      // for each of the rules set in the switchIf key
-      (rule) => {
-        // we check if each of the condition are fulfilled
-        return Object.keys(rule).find((value, _index, _obj) => {
-          return $stores[props.type].filters[value].multiple
-            ? $stores[props.type].filters[value]?.value &&
-                $stores[props.type].filters[value]?.value.includes(rule[value])
-            : $stores[props.type].filters[value]?.value === rule[value]
-        })
-      },
-    )
-  )
+// const computeVisibility = (filterItem) => {
+//   return (
+//     // if anything is set in the visibility key
+//     !$stores[props.type].filters[filterItem].visibility ||
+//     $stores[props.type].filters[filterItem].visibility?.default ||
+//     $stores[props.type].filters[filterItem].visibility?.switchIf.find(
+//       // for each of the rules set in the switchIf key
+//       (rule) => {
+//         // we check if each of the condition are fulfilled
+//         return Object.keys(rule).find((value, _index, _obj) => {
+//           return $stores[props.type].filters[value].multiple
+//             ? $stores[props.type].filters[value]?.value &&
+//                 $stores[props.type].filters[value]?.value.includes(rule[value])
+//             : $stores[props.type].filters[value]?.value === rule[value]
+//         })
+//       },
+//     )
+//   )
+// }
+
+const computeVisibility = (filterKey) => {
+  const filters = $stores[props.type].filters
+  const show = filters?.[filterKey]?.show
+
+  if (!show) return true
+  if (show.default) return true
+  if (!Array.isArray(show.switchIf)) return false
+
+  const checkRule = (rule) =>
+    Object.entries(rule).every(([depKey, expected]) => {
+      const dep = filters?.[depKey]
+      if (!dep) return false
+      const cur = dep.value
+
+      if (dep.multiple) {
+        if (!Array.isArray(cur)) return false
+        return Array.isArray(expected)
+          ? expected.some((e) => cur.includes(e))
+          : cur.includes(expected)
+      }
+      return Array.isArray(expected) ? expected.includes(cur) : cur === expected
+    })
+
+  return show.disjonctive !== false
+    ? show.switchIf.some(checkRule)
+    : show.switchIf.every(checkRule)
 }
 </script>
 
