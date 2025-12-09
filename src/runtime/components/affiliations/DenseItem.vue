b5c691158cc57c09e71d107038f477d2671ff6e1
billybobbonnet <boitalettre9@gmail.com>
2025-12-09 12:10:01 +0100

    chore: bumped trees package version

---
+++ src/runtime/components/affiliations/DenseItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row class="my-2 highlight-on-hover" no-gutters>
+    <v-col cols="12" class="px-4">
+      <v-skeleton-loader v-if="isLoading" type="heading, text@6" />
+      <template v-else>
+        <div class="text-h5">{{ item.name }}</div>
+        <div v-if="item.summary" class="text-body-1 mt-1">
+          <MDC :value="item.summary" />
+        </div>
+      </template>
+    </v-col>
+  </v-row>
+  <v-divider />
+</template>
+
+<script setup>
+import { computed } from "#imports"
+import { useRootStore } from "../../stores/root"
+
+const rootStore = useRootStore()
+const props = defineProps({
+  item: { type: Object, required: true },
+  loading: { type: Boolean, default: false },
+})
+
+const isLoading = computed(() => rootStore.loading || props.loading)
+</script>
+
+<style></style>
---
+++ src/runtime/components/affiliations/RowsItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row class="highlight-on-hover pa-3" no-gutters>
+    <v-col cols="12" class="px-6">
+      <v-skeleton-loader v-if="isLoading" type="heading, text@8, button" />
+      <template v-else>
+        <div class="text-h5">{{ item.name }}</div>
+        <div v-if="item.summary" class="mt-2">
+          <MDC :value="item.summary" />
+        </div>
+      </template>
+    </v-col>
+  </v-row>
+  <v-divider />
+</template>
+
+<script setup>
+import { computed } from "#imports"
+import { useRootStore } from "../../stores/root"
+
+const rootStore = useRootStore()
+const props = defineProps({
+  item: { type: Object, required: true },
+  loading: { type: Boolean, default: false },
+})
+
+const isLoading = computed(() => rootStore.loading || props.loading)
+</script>
+
+<style></style>
