93f14d32cca543dadc2245d5069315aae27e8c93
Hamed Bouare <hamed.bouare@paris-iea.fr>
2026-01-06 20:23:27 +0100

    feat: improve usefetchItem

--- src/runtime/composables/useFetchItem.ts
+++ src/runtime/composables/useFetchItem.ts
@@ -1,76 +1,4 @@
-import { useRoute, useI18n, useSetI18nParams, useNuxtApp } from "#imports"
-
-export const useFetchItem = () => {
-  const fetchItem = async <T>(payload: {
-    query: string
-    key: string
-  }): Promise<T> => {
-    try {
-      const { locale } = useI18n()
-      const route = useRoute()
-      const { $apollo } = useNuxtApp()
-      console.log("query: ", payload.query)
-      console.log("key: ", payload.key)
-
-      const variables = {
-        itemId: route.params.slug?.toString().trim(),
-        appId: "iea",
-        lang: locale.value,
-      }
-
-      // Use the Apollo client directly via the clients property
-      const apolloClient = $apollo?.clients?.default
-
-      if (!apolloClient) {
-        throw new Error("Apollo client is not available")
-      }
-      console.log("variables: ", variables)
-
-      const { data } = await apolloClient.query({
-        query: payload.query,
-        variables,
-      })
-      /*  console.log("variables: ", variables) */
-      console.log("data: ", data)
-
-      const item = data?.[payload.key]
-      /*      console.log("item: ", item) */
-
-      if (!item) {
-        throw new Error("Item not found in response")
-      }
-      // Update the slug in the i18n params
-      // for people, the slug is the same in both languages
-      // for other items, the slug is different in each language
-      const setI18nParams = useSetI18nParams()
-      if (!route.name.includes("people")) {
-        /*  console.log("update params") */
-        setI18nParams({
-          en: { slug: item.slug.en },
-          fr: { slug: item.slug.fr },
-        })
-      } else {
-        // for people, the slug is the same in both languages
-        setI18nParams({
-          en: { slug: item.slug },
-          fr: { slug: item.slug },
-        })
-      }
-      return item as T
-    } catch (error) {
-      console.error("Error fetching item:", error)
-      throw new Error(
-        `Item not found: ${
-          error instanceof Error ? error.message : String(error)
-        }`,
-      )
-    }
-  }
-
-  return {
-    fetchItem,
-  }
-}
+import { useRoute, useSetI18nParams, useNuxtApp } from "#imports"
 
 type FetchItemPayload = {
   query: any
@@ -78,12 +6,13 @@ type FetchItemPayload = {
   variables?: Record<string, any>
 }
 
-export const useFetchItem2 = () => {
+export const useFetchItem = () => {
   const fetchItem = async <T>({
     query,
     key,
     variables,
   }: FetchItemPayload): Promise<T> => {
+    console.log("useFetchItem2 called with:", { query, key, variables })
     try {
       const { $apollo } = useNuxtApp()
       const route = useRoute()
